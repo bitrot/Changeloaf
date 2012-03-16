0b44010c4471d2ec86a9c1fc33108b80f0688a76
Ryan Macy <me@rmacy.us>
2012-03-16 14:34:48 -0400

    fixed schema issues
    Signed-off-by: Ryan Macy <me@rmacy.us>

--- db/schema.js
+++ db/schema.js
@@ -8,6 +8,6 @@ var Link = describe('Link', function () {
       property('title', String);
       property('url', String);
       property('user', Number);
-      property('date', Date );
+      property('date', Date);
       property('private', Boolean);
 });
